c92e7b61fed41b2d9d59d96027f51c88ccd5a4f2
Mladen Bajic and Ryan Spore <pair+mladen+rspore@pivotal.io>
2014-11-25 17:55:56 -0800

    Add Look SDK to packages in to acquire for shadowing

--- robolectric/src/main/java/org/robolectric/bytecode/Setup.java
+++ robolectric/src/main/java/org/robolectric/bytecode/Setup.java
@@ -94,6 +94,7 @@ public class Setup {
         || className.startsWith("com.google.android.gms.")
         || className.startsWith("com.google.android.youtube.")
         || className.startsWith("dalvik.system.")
+        || className.startsWith("com.samsung.android.sdk.look.")
         || className.startsWith("org.apache.http.impl.client.DefaultRequestDirector");
   }
 
